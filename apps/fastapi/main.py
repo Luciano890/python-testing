4d2ca39c52e89a0818a4ea92043489049aa9bbed
Luciano Chavarria <62488915+Luciano890@users.noreply.github.com>
2022-08-30 18:14:27 +0000

    feat: added docker files to fastapi apps

--- apps/fastapi/main.py
+++ apps/fastapi/main.py
@@ -1,9 +1,16 @@
 """
 A simple FastAPI application.
 """
-from fastapi import FastAPI # pylint: disable=import-error
+# pylint: disable=import-error
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Spartan(BaseModel):
+    name: str
+    age: int
 
 @app.get("/")
 async def root():
@@ -13,3 +20,18 @@ async def root():
         dict: message response
     """
     return {"message": "Hello World"}
+
+@app.get("/spartans")
+async def get_spartans():
+    """Get all spartans
+
+    Returns:
+        list: list of spartans
+    """
+    return [
+        {"name": "John", "age": 30},
+        {"name": "Doe", "age": 25},
+    ]
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
